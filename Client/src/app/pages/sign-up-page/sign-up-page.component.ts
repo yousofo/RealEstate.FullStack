0d8f567451fdb89e211f1f53f4fa7adb6137dd60
yousofo <jonabilmc@gmail.com>
2025-03-15 01:13:38 +0200

    added signup page boilerplate

---
+++ Client/src/app/pages/sign-up-page/sign-up-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up-page',
+  standalone: false,
+  templateUrl: './sign-up-page.component.html',
+  styleUrl: './sign-up-page.component.css'
+})
+export class SignUpPageComponent {
+
+}
